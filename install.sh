7537baafc4765e893cda9f2576755cdc75f8ddf8
h4ckm1n-Dev <mayer.julien22@gmail.com>
2022-12-07 13:14:48 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,12 @@ sudo apt-get install zsh neofetch lolcat figlet
 #Install startship prompt
 curl -sS https://starship.rs/install.sh | sh
 #Copy dotfiles to correct directory
+mkdir .config
 cp ./zshrc /home/$USER/.zshrc
 cp ./starship.toml /home/$USER/.config/
-
-echo 'Installation complete please restart terminal'
+cd .config
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+cd
+echo 'Installation complete, Please use "chsh" command and type "/bin/zsh" please restart terminal'
--- zshrc
+++ zshrc
@@ -1,6 +1,8 @@
 # ~/.zshrc file for zsh interactive shells.
 # see /usr/share/doc/zsh/examples/zshrc for examples
+
 source ~/.config/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+
 setopt autocd              # change directory just by typing its name
 setopt correct            # auto correct mistakes
 setopt interactivecomments # allow comments in interactive mode
