d9eca975693be252ec70d0dddf9a7444d7e275be
h4ckm1n-dev <mayer.julien22@gmail.com>
2023-01-08 21:41:27 +0100

    Changes to be committed: 	modified:   install.sh

--- install.sh
+++ install.sh
@@ -7,12 +7,11 @@ sudo apt-get install zsh neofetch lolcat figlet curl
 #Install startship prompt
 curl -sS https://starship.rs/install.sh | sh
 #Copy dotfiles to correct directory
-mkdir .config
+mkdir ~/.config
 cp ./zshrc /home/$USER/.zshrc
 cp ./starship.toml /home/$USER/.config/
-cd .config
+cd ~/.config
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 cd
 echo 'Installation complete, Please use "chsh" command and type "/bin/zsh" please restart terminal'
