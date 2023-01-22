3b62886d153e2d1bcfb54ddaf784b8264a92d23a
h4ckm1n <97511408+h4ckm1n-dev@users.noreply.github.com>
2023-01-22 20:35:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,34 @@
 #/bin/bash
 
 # Determine the OS version
-OS=$(lsb_release -si)
-VERSION=$(lsb_release -sr)
+if command -v lsb_release > /dev/null; then
+  OS=$(lsb_release -si)
+  VERSION=$(lsb_release -sr)
+elif [ -f /etc/os-release ]; then
+  OS=$(grep ^ID= /etc/os-release | cut -f 2- -d =)
+  VERSION=$(grep ^VERSION_ID= /etc/os-release | cut -f 2- -d =)
+elif [ -f /etc/arch-release ]; then
+  OS="Arch"
+  VERSION=$(pacman -Q archlinux | awk '{print $2}')
+elif [ -f /etc/fedora-release ]; then
+  OS="Fedora"
+  VERSION=$(rpm -q --queryformat '%{VERSION}' fedora-release)
+elif [ -f /etc/redhat-release ]; then
+  OS=$(cat /etc/redhat-release | awk '{print $1}')
+  VERSION=$(cat /etc/redhat-release | awk '{print $3}')
+elif [ -f /etc/raspbian-release ]; then
+  OS="Raspbian"
+  VERSION=$(cat /etc/raspbian-release | awk '{print $2}')
+elif [ -f /etc/kali-version ]; then
+  OS="Kali"
+  VERSION=$(cat /etc/kali-version | awk '{print $1}')
+elif [ -f /etc/manjaro-release ]; then
+  OS="Manjaro"
+  VERSION=$(cat /etc/manjaro-release | awk '{print $2}')
+else
+  echo "This script does not support your OS."
+  exit 1
+fi
 
 # Install dependencies for each OS
 if [ "$OS" == "CentOS" ] || [ "$OS" == "RedHat" ]; then
