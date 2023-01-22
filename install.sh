449bc6d0191b3a1c1dfd11b5c10d713f63be7f6b
h4ckm1n <97511408+h4ckm1n-dev@users.noreply.github.com>
2023-01-22 19:50:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,5 +13,5 @@ cp ./starship.toml /home/$USER/.config/
 cd ~/.config
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-cd
+cd ~/
 echo 'Installation complete, Please use "chsh" command and type "/bin/zsh" then restart terminal'
