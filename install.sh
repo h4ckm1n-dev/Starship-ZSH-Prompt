bdc9f370f6c676a48116d3b410a11c54887a1de7
h4ckm1n <97511408+h4ckm1n-dev@users.noreply.github.com>
2023-01-25 00:50:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -84,5 +84,6 @@ cp -r ./starship.toml ~/.config/
 mkdir -p ~/.config/zsh
 git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/zsh-autosuggestions
 git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/zsh-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
 
 echo 'Installation complete. Please restart your terminal to start using the new prompt.'
